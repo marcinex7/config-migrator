8b48947698e2ed7b2216c7276274532a663a0c25
Marcin Podolski <marcin.podolski@gft.com>
2022-12-13 12:23:34 +0100

    gradle setup

---
+++ README.md
@@ -0,0 +1,17 @@
+# Groovy to YAML converter
+
+## Requirements
+
+* Groovy 4.0.0
+
+## Build and run
+
+```shell
+./gradlew migrate -Pfile=<ABSOLUTE_PATH_TO_CONFIG_FILE>
+```
+
+example:
+
+```shell
+./gradlew migrate -Pfile="/Users/e-mnps/Projects/config-migrator/src/main/resources/config.groovy"
+```
--- src/main/groovy/migrator.groovy
+++ src/main/groovy/migrator.groovy
@@ -1,18 +1,28 @@
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLMapper
+import org.apache.commons.io.FileUtils
+import org.apache.commons.io.FilenameUtils
 import pl.jalokim.propertiestojson.util.PropertiesToJsonConverter
 
-def configGroovy = new File("/Users/e-mnps/Projects/config-migrator/src/main/resources", "config.groovy")
+import java.nio.file.Path
+
+def configPath = args[0]
+
+def configGroovy = Path.of(configPath).toFile()
+if (configPath.isBlank() || !configGroovy.exists()) {
+    println "Incorrect config file path!"
+    println "Usage: './gradlew migrate -Pfile=<ABSOLUTE_PATH_TO_CONFIG_FILE>"
+    System.exit(0)
+}
 
 def config = new ConfigSlurper("production").parse(configGroovy.text).toProperties().sort(Comparator.comparing {it.toString().toLowerCase()})
 
 def json = new PropertiesToJsonConverter().convertToJson(config)
-
 def jsonNode = new ObjectMapper().readTree(json)
-
 def yaml = new YAMLMapper().writeValueAsString(jsonNode)
 
-new File("/Users/e-mnps/Projects/config-migrator/src/main/resources", "config.json").write(json)
-new File("/Users/e-mnps/Projects/config-migrator/src/main/resources", "config.yaml").write(yaml)
+def fullPath = FilenameUtils.getFullPath(configPath)
+def baseName = FilenameUtils.getBaseName(configPath)
 
-println yaml
+Path.of(fullPath, "${baseName}.json").toFile().write(json)
+Path.of(fullPath, "${baseName}.yaml").toFile().write(yaml)
